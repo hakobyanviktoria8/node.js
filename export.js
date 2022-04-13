c1ad64f1b544649b31778b3da7e1edb8867955fd
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-13 16:59:04 +0400

    Module exports 3 way

---
+++ export.js
@@ -0,0 +1,7 @@
+// const car = require("./export_file")
+// const item= require("./export_file")
+const {tree}= require("./export_file")
+
+// console.log(car);
+// console.log(item.tree)
+console.log(tree)
