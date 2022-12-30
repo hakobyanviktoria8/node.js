79d33f4d1a36f489df327895afc5c1572ce73cbd
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-12-30 16:33:04 +0400

    Promise All Try Catch Finaly

--- lesson3/index.js
+++ lesson3/index.js
@@ -1,6 +1,6 @@
 const os = require("os")
 const generateFileName = require("./file-name")
-const fs = require("fs")
+const fs = require("fs").promises
 
 // console.log(os.totalmem());
 // console.log(os.networkInterfaces());
@@ -14,7 +14,6 @@ const network = os.networkInterfaces();
 // })
 
 // read input.txt file text and write output.tx file
-
 // fs.readFile("./input.txt", function(err, data){
 //     let newText = data.toString().split(" ").join("-")
 
@@ -24,10 +23,42 @@ const network = os.networkInterfaces();
 // })
 
 
-fs.writeFile(
-    `${os.platform()}.txt`, 
-    `Hello new life ${new Date().getDate()}`, 
-    function(err){
-        console.log(err?.message);
+// fs.writeFile(
+//     `${os.platform()}.txt`, 
+//     `Hello new life ${new Date().getDate()}`, 
+//     function(err){
+//         console.log(err?.message);
 
+// })
+
+// async function foo() {
+//     try {
+//         let data =  await fs.readFile("win32.txt")
+//         console.log(data.toString());
+//         await fs.writeFile("111111.txt", data.toString().toUpperCase())        
+//     } catch (error) {
+//         console.log(error.message);
+//     } finally {
+//         console.log("Go");
+//     }
+// }
+// foo()
+
+// promis All
+Promise.all([
+    fs.readFile("./input.txt"),
+    fs.readFile("./output.txt"),
+]).then(function(data){
+    console.log(data.toString());
+    fs.writeFile("./mixContentPromisAll.txt", data.toString())
+    Promise.all([
+        fs.writeFile("pAll1.txt", data[0].toString()),
+        fs.writeFile("pAll2.txt", data[1].toString())
+    ])
+}).then(function(){
+    console.log("Ok");
+}).catch(function(err){
+    console.log(err.message);
+}).finally(function(){
+    console.log("finaly");
 })
