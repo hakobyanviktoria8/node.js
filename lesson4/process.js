19c8d47e847a7a3b8256f1db2d91eb30ae1de0a9
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-01-11 17:50:05 +0400

    work with process on, env, nextTick

---
+++ lesson4/process.js
@@ -0,0 +1,21 @@
+process.on("exit", function(){
+    console.log("exit");
+})
+console.log(process.env.NODE_ENV)
+
+// ___we use production and development____
+const config = require("./../config.json")[process.env.NODE_ENV]
+const http = require("http")
+
+
+const server = http.createServer((req,res) => {
+    res.end("OK")
+})
+
+server.listen(config?.port, config?.host)
+
+
+// nextTick done when find stack empty, before logs
+process.nextTick(()=>{
+    console.log("nextTick")
+});
