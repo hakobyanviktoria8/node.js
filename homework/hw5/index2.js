2df7e8c278ca361ea3435ebe40dfc07e2e2efd00
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-01-12 15:35:28 +0400

    use Transform readData, change it and write another file

--- homework/hw5/index2.js
+++ homework/hw5/index2.js
@@ -1,4 +1,5 @@
 const fs = require("fs")
+const {Transform} = require("stream")
 
 const readData =  fs.createReadStream("./input.txt", {
     encoding: "utf-8"
@@ -6,10 +7,19 @@ const readData =  fs.createReadStream("./input.txt", {
 
 const writeData = fs.createWriteStream("./output2.txt")
 
-readData.on("data", (chunk) => {
-    console.log([...chunk].map((x,i) => chunk.charCodeAt(i)))
-    writeData.write([...chunk].map((x,i) => chunk.charCodeAt(i)).join(","))
+// readData.on("data", (chunk) => {
+//     console.log([...chunk].map((x,i) => chunk.charCodeAt(i)))
+//     writeData.write([...chunk].map((x,i) => chunk.charCodeAt(i)).join(","))
+// })
+
+const transform = new Transform({
+    transform(chunk, encoding, callback){
+        this.push([...chunk.toString()].map((x,i) => chunk.toString().charCodeAt(i)).join(","))
+        callback()
+    }
 })
+
+readData.pipe(transform).pipe(writeData)
 
 readData.on("end", ()=> {
     console.log("end");
