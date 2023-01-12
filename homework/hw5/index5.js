cf49e7fd51eeaed416207a03dcfcd35027be07a1
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-01-12 21:13:34 +0400

    read file data make 1later uppercase with Promise

---
+++ homework/hw5/index5.js
@@ -0,0 +1,12 @@
+const fs = require("fs").promises
+
+async function func1(){
+    const data = await (await fs.readFile("./input.txt")).toString()
+    console.log(data, 1111111);
+    const writeData = await fs.writeFile("./output5.txt", 
+    data.split(" ").map(x=>x[0].toUpperCase().concat(x.slice(1))).join(" "))
+}
+
+func1()
+.then(()=>console.log("Done"))
+.catch(err => console.log(err?.message))
