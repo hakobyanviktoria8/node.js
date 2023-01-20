f1c2df7c96c7937020f7cd38b547802426a3356c
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-01-20 17:35:18 +0400

    read dir, give files, move them new dir, write inside test

--- homework/hw6/index4.js
+++ homework/hw6/index4.js
@@ -1,10 +1,18 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
 async function readAndMove(dirName) {
-  const readData = await fs.readFile(dirName);
-  console.log(111, readData);
+  const readData = await fs.readdir(dirName);
+  await fs.mkdir(`${dirName}_111`, (err) => {
+    err && console.log(err.message);
+    console.log("Done mkdir");
+  });
+  await readData.map((item) =>
+    fs.writeFile(`${dirName}_111/${item}`, "test", (err, data) => {
+      err && console.log(err);
+    })
+  );
 }
 
 readAndMove("./web1")
   .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err.message));
