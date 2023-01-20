a9b14b79651e1b47a29edea38943143e51fbc9a9
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-01-20 18:46:50 +0400

    with fsExtra npm move file new place, with content

--- homework/hw6/index4_fsExtra.js
+++ homework/hw6/index4_fsExtra.js
@@ -3,18 +3,14 @@ const fs = require("fs").promises;
 
 async function moveDir(dirName) {
   const files = await fs.readdir(dirName);
-  await fs.mkdir("./newDirectory");
 
   for (let file of files) {
-    const start = await fs.stat(`${dirName}/${file}`);
+    const stat = await fs.stat(`${dirName}/${file}`);
 
-    if (start.isFile() && start.size > 1) {
-      console.log("Done", 1);
-      await fs.rename(`${dirName}/${file}`, `./newDirectory/${file}`);
-
-      console.log("Done", 2);
+    if (stat.isFile() && stat.size > 1) {
+      await fsExtra.move(`${dirName}/${file}`, `./newDirectory/${file}`);
     }
   }
 }
 
-moveDir("./web1").catch((err) => console.error(err));
+moveDir("./web").catch((err) => console.error(err));
