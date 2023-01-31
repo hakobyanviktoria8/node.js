9bad25d9701c89ac16c0587860126a1b7dfe337e
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-02-01 00:53:13 +0400

    worked with postman POST data, and render it in UI

--- advansedNode.js/express/index.js
+++ advansedNode.js/express/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-// console.log(app);
+const messages = [
+  { name: "Viki", message: "Hello new day" },
+  { name: "Ani", message: "Hello new life" },
+];
+
 app.use(express.static(__dirname));
-app.get("/message", (req, res) => {
-  res.send("Hello");
+app.use(bodyParser.json());
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  // console.log("body", req.body);  //body { name: 'Viktorya', message: 'Hello new shanse' }
+  messages.push(req.body);
+  res.sendStatus(200);
 });
 
 const server = app.listen(3003, () => {
-  console.log("server is runing", server.address().port);
+  console.log("server is runing", server.address().port); //server is runing 3003
 });
