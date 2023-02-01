4a85adbaa9a22eea694453d82a52aa54c59f3e59
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-02-01 23:06:58 +0400

    use socket.io to send 2-nd browser message and see that 1-ts browser

--- advansedNode.js/express/index.js
+++ advansedNode.js/express/index.js
@@ -20,6 +20,7 @@ app.get("/messages", (req, res) => {
 app.post("/messages", (req, res) => {
   // console.log("body", req.body);  //body { name: 'Viktorya', message: 'Hello new shanse' }
   messages.push(req.body);
+  io.emit("message", req.body);
   res.sendStatus(200);
 });
 
