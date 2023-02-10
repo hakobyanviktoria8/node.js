2c6afe92e0c3bbc74184e7c90d13f21790986249
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-02-11 00:34:07 +0400

    EventEmitter with on and emit methods

---
+++ lesson7/eventEmitter.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const { EventEmitter } = require("events");
+
+const customEvent = new EventEmitter();
+
+customEvent
+  .on("start", () => {
+    console.log("start 1");
+  })
+  .on("start", (n) => {
+    console.log("start 2", n);
+  })
+  .emit("start", 2);
+
+// var http = require("http");
+// http
+//   .createServer(function (req, res) {
+//     console.log("Request for " + req.method + " received.");
+
+//     // Print the name of the file for which request is made.
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.end(`<form action="/" method="Post">
+//         <textarea name="content" id="" cols="30" rows="10"></textarea>
+//         <input type="submit" name="" >
+//     </form>`);
+//   })
+//   .listen(3000);
