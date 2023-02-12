6eabe7bccd6f0e98fdd2f735d5d8f89255a380f5
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-02-12 20:11:48 +0400

    useing express app.use req.query or req.body

--- lesson7/express.js
+++ lesson7/express.js
@@ -5,6 +5,17 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  console.log("log", req.query); //log { id: '2' } query ...?id=2
+  //postman Body -> x-www-form-urlencoded
+  console.log(req.body); //[Object: null prototype] { data: 'secret' }
+  next();
+});
+app.use("/posts", (req, res, next) => {
+  res.write("Posts page");
+  next();
+});
 app.use("/about", (req, res, next) => {
   res.end("About page");
 });
