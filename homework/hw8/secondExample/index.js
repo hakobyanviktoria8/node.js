d80adbeb4c6abea3473bdd24aaedf83032a80cc9
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-02-17 02:11:21 +0400

    req.body data read undefined error fixed

--- homework/hw8/secondExample/index.js
+++ homework/hw8/secondExample/index.js
@@ -2,9 +2,24 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
-router.route("/posts").all((req, res) => {
-  res.send(`Met ${req.method}, ${new Date().toDateString()}`);
-});
+// req.body is undefined, we i body-parser
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// or express.encoded then json
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+router
+  .route("/posts")
+  .get((req, res) => {
+    res.send(`Met ${req.method}, ${new Date().toDateString()}`);
+  })
+  .post((req, res) => {
+    console.log(req.body);
+    res.send(`Met ${req.method}, ${new Date().toDateString()}`);
+  });
 
 router.route("/posts/:uid").put((req, res) => {
   console.log(`User id: ${req.params.uid}`);
