ed85fe3c8a095a717a01e362315cda631a02c2e6
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-02-17 01:26:48 +0400

    create dinamic route :id for varible

--- homework/hw8/secondExample/index.js
+++ homework/hw8/secondExample/index.js
@@ -6,6 +6,11 @@ router.route("/posts").all((req, res) => {
   res.send(`Met ${req.method}, ${new Date().toDateString()}`);
 });
 
+router.route("/posts/:uid").put((req, res) => {
+  console.log(`User id: ${req.params.uid}`);
+  res.send("OK");
+});
+
 app.use(router);
 
 app.listen(3333, function () {
